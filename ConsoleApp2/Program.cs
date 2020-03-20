114aa84d83735d460ec2a8ecf97a7e10905221b7
1720934884@qq.com <1720934884@qq.com>
2020-03-20 16:06:26 +0800

    THREAD

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,6 +11,10 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("1708A");
+
+
+            Console.WriteLine("1708C");
+
             Console.WriteLine("1708B");
         }
     }
